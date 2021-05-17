290b4f4b4988fd352f59783d666292786d823663
Muhammad Umair <hafizumairtariq1234@gmail.com>
2021-05-17 23:15:14 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,87 @@
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Red Queen's Race</h1>
+    <div className="wrapper">
+      <div className="sky"></div>
+      <div className="earth">
+        <div id="red-queen_and_alice">
+          <img
+            id="red-queen_and_alice_sprite"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
+            alt="Alice and the Red Queen running to stay in place."
+          />
+        </div>
+      </div>
+
+      <div className="scenery" id="foreground1">
+        <img
+          id="palm3"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
+          srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"
+          alt=" "
+        />
+      </div>
+
+      <div className="scenery" id="foreground2">
+        <img
+          id="bush"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"
+          srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x"
+          alt=" "
+        />
+        <img
+          id="w_rook_upright"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png"
+          srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x"
+          alt=" "
+        />
+      </div>
+
+      <div className="scenery" id="background1">
+        <img
+          id="r_pawn_upright"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
+          srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x"
+          alt=" "
+        />
+        <img
+          id="w_rook"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"
+          srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x"
+          alt=" "
+        />
+        <img
+          id="palm1"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
+          srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x"
+          alt=" "
+        />
+      </div>
+
+      <div className="scenery" id="background2">
+        <img
+          id="r_pawn"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png"
+          srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x"
+          alt=" "
+        />
+        <img
+          id="r_knight"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png"
+          srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x"
+          alt=" "
+        />
+        <img
+          id="palm2"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png"
+          srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"
+          alt=" "
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
